7f07320af8cda81e6eb4a72f53e16c5ac45e580c
lakshmi87ambati <lt.ambati@gmail.com>
2021-02-13 06:26:39 +0000

    FIRST COMMIT

---
+++ com.studycase.tulasi/src/main/java/com/studycase/completed/HelloController.java
@@ -0,0 +1,11 @@
+package com.studycase.completed;
+
+
+public class HelloController {
+	
+	public static  void test() {
+		System.out.println("hellocontrolller.java");
+		
+	}
+
+}
---
+++ com.studycase.tulasi/src/test/java/com/studycase/completed/HelloControllerTest.java
@@ -0,0 +1,18 @@
+package com.studycase.completed;
+
+
+import org.junit.Test;
+
+
+
+
+public class HelloControllerTest {
+    
+
+    @Test
+    public void testHello() throws Exception {
+       System.out.println("casestudy 1,2 and 3 are completed to run");
+    }
+
+   
+}
